18824cecdcf0816ac0e142c7892a607255d1df3b
Krasimir Stefanov <kraskoo@users.noreply.github.com>
2016-06-17 16:54:06 +0300

    Predicate for names

---
+++ Functional Programming/P07PredicateForNames/PredicateForNames.cs
@@ -0,0 +1,19 @@
+﻿namespace P07PredicateForNames
+{
+    using System;
+    using System.Linq;
+
+    public class PredicateForNames
+    {
+        public static void Main()
+        {
+            int length = int.Parse(Console.ReadLine());
+            Predicate<string> nameValidator = n => n.Length <= length;
+            Console.ReadLine()?
+                .Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Where(n => nameValidator(n))
+                .ToList()
+                .ForEach(Console.WriteLine);
+        }
+    }
+}
